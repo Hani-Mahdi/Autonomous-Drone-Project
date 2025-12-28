3c4614c34443afacb8deca59cd50c704a6a165b3
Hani-Mahdi <traebrownnbw@gmail.com>
2025-12-28 18:25:14 -0500

    initial commit

---
+++ fsm/states.py
@@ -0,0 +1,62 @@
+'''
+Docstring for fsm.states
+
+States = 
+
+- diagnostics
+- Takeoff
+- Flight
+ - Search & Detect
+ - To Target
+ - Drop Off
+    - Descent
+    - Release
+    - Ascent
+- Landing
+- Emergency Landing
+
+
+-----------------------------------------------------------------------------------------------------------------
+Function: Diagnostics
+Input: Battery Percentage, Camera Health, Payload Gate Check, sensor health, Motor Check
+
+Execute: 
+- Read battery percentage
+- Check camera output
+- Open and close payload gate to test
+- Check how many sensors are picking up signal
+- Quickly turn on and off motors (good speed but not fast enough to fly)
+
+Output: {
+    battery_p: [xx%, "Battery Healthy"],
+    camera_health: [Bool, "Camera Operational"],
+    payload-gate: [Bool, "Payload Gate: (Operational or review required) "],
+    sensor_health: [Bool, "Sensors working: x/2"]
+}
+-----------------------------------------------------------------------------------------------------------------
+Function: Takeoff
+Input: Battery Percentage, altitude, take_off_start, current_time, prev5_alts
+
+Execute:
+
+- Activate thrust and altitude control controllers (with altitude x)
+- check altitude
+- is altitude the same as past few checks and = x
+    - yes: state -> Flight
+        break
+
+abort if alt wasnt reached within threshold (current_time - take_off_start > threshold)
+     
+Output: {
+    status: "Complete" | "Abort"
+    issue : "none" | "speed issue" | "Cant reach alt" | "cant hold alt"
+}
+-----------------------------------------------------------------------------------------------------------------
+Function: Flight
+Input: Battery Percentage, altitude, take_off_start, current_time
+
+Execute:
+
+
+        
+'''
